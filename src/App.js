3641d2e369b4569288f0523090e929cdd6b01a29
edilson@Acer-E5-473 <ed.vilarinho@gmail.com>
2021-12-19 00:44:24 -0300

    Aula 17 Primeiro Componente #01

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Text } from 'react-native'
+//import { View, StyleSheet, SafeAreaView } from "react-native";
+
+function App() {    
+    //const jsx = <Text> Primeiro Componente 2 </Text> 
+    //return jsx
+    return <Text> Primeiro Componente 2 </Text>
+}
+
+export default App
+
+//<SafeAreaView>
+
+
+//</SafeAreaView>
